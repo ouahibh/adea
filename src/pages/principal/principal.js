ce30d418d456f8e25f294b09539d7960f526b7f7
ouahibh <sofiane.h.06.b@gmail.com>
2021-01-17 19:34:56 +0100

    My Fisrt commit Adea

---
+++ src/pages/principal/principal.js
@@ -0,0 +1,17 @@
+import './principal.css'
+import ResponsiveDrawer from '../../components/drawer/drawer';
+
+
+
+const  Principal = ()=>{
+    
+    return(
+        <div className={'principl-C'}>
+            
+            <div className={'drawer'}>
+            <ResponsiveDrawer/>
+            </div>
+        </div>
+    )
+}
+export default Principal
